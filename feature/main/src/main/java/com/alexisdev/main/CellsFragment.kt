7e44b14f725c73f222f7882534177d4330ece1ac
Alexey <Alex.Zykin15@gmail.com>
2024-08-16 02:59:45 +0700

    add coroutine scope extension

--- feature/main/src/main/java/com/alexisdev/main/CellsFragment.kt
+++ feature/main/src/main/java/com/alexisdev/main/CellsFragment.kt
@@ -1,22 +1,15 @@
 package com.alexisdev.main
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
-import androidx.recyclerview.widget.RecyclerView.VERTICAL
 import com.alexisdev.main.databinding.FragmentCellsBinding
-import kotlinx.coroutines.launch
+import com.alexisdev.main.utils.launchUntilPaused
 import org.koin.androidx.viewmodel.ext.android.viewModel
-
 
 class CellsFragment : Fragment() {
     private lateinit var binding: FragmentCellsBinding
@@ -33,26 +26,25 @@ class CellsFragment : Fragment() {
         return binding.root
     }
 
-    @SuppressLint("UnsafeRepeatOnLifecycleDetector")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.rvCells.layoutManager = LinearLayoutManager(context)
         binding.rvCells.adapter = adapter
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.getCellsUiState().collect {
-                    when (it) {
-                        is CellsUiState.Loading -> {}
-                        is CellsUiState.Error -> {}
-                        is CellsUiState.Content -> {
-                            adapter.map(it.cells)
-                        }
+        binding.btCreateCell.setOnClickListener {
+            viewModel.onEvent(CellsUiEvent.OnCreateNewCell)
+        }
+        lifecycleScope.launchUntilPaused(this) {
+            viewModel.getCellsUiState().collect {
+                when (it) {
+                    is CellsUiState.Loading -> {}
+                    is CellsUiState.Error -> {}
+                    is CellsUiState.Content -> {
+                        adapter.map(it.cells)
                     }
                 }
             }
         }
-        binding.btCreateCell.setOnClickListener {
-            viewModel.onEvent(CellsUiEvent.OnCreateNewCell)
-        }
     }
 }
+
+
